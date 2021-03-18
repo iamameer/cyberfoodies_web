bf83d5a250ab32944ea6e07757f4e8c93f96f7f6
iamameer <ameersorne@gmail.com>
2021-03-18 19:26:50 +0800

    fixed index/twitter feeds

--- index.php
+++ index.php
@@ -89,23 +89,7 @@
     <!-- Women Banner Section Begin -->
     <section class="women-banner spad" style="margin-top:10px;">
         <div class="container-fluid">
-            <div class="row">  
-                <div class="col-lg-3">
-                    <!-- Switch Mode
-                 <div class="onoffswitch">
-                        <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" 
-                        onchange='handleChange' id="myonoffswitch" tabindex="0">
-                        <label class="onoffswitch-label" for="myonoffswitch"> </label> 
-                    </div>  -->
-                <a class="twitter-timeline" data-theme="light" id="toggle" height="650px"
-                href="https://twitter.com/Twt_Cyberjaya?ref_src=twsrc%5Etfw">Tweets by Twt_Cyberjaya</a> 
-                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                    <!-- <div class="product-large set-bg" data-setbg="img/pasar.jpg">
-                        <h2 style="text-shadow: 3px 3px 11px rgba(128, 0, 0, 1);">Bazaar</h2>
-                        <a href="bazaar.php" style="text-shadow: 3px 3px 11px rgba(0, 0, 0, 1);">Special promotion during bazaar?</a>
-                    </div> -->
-                    
-                </div>
+            <div class="row">   
                 <div class="col-lg-8 offset-lg-1">
                     <div class="filter-control">
                         <!-- <ul>
@@ -154,6 +138,31 @@
                        
                     </div>
                 </div>
+
+                <div class="col-lg-3">
+                    <style type="text/css">
+                        .timeline-Widget,.twitter-timeline{ 
+                            box-shadow: 10px 10px 62px -13px rgba(88,200,248,0.75)!important;
+                            -webkit-box-shadow: 10px 10px 62px -13px rgba(88,200,248,0.75)!important;
+                            -moz-box-shadow: 10px 10px 62px -13px rgba(88,200,248,0.75)!important;
+                        }
+                    </style>
+                    <!-- Switch Mode
+                    <div class="onoffswitch">
+                            <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" 
+                            onchange='handleChange' id="myonoffswitch" tabindex="0">
+                            <label class="onoffswitch-label" for="myonoffswitch"> </label> 
+                        </div>  -->
+                    <a class="twitter-timeline" data-theme="light" id="toggle" height="650px"
+                    href="https://twitter.com/Twt_Cyberjaya?ref_src=twsrc%5Etfw">Tweets by Twt_Cyberjaya</a> 
+                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                        <!-- <div class="product-large set-bg" data-setbg="img/pasar.jpg">
+                        <h2 style="text-shadow: 3px 3px 11px rgba(128, 0, 0, 1);">Bazaar</h2>
+                        <a href="bazaar.php" style="text-shadow: 3px 3px 11px rgba(0, 0, 0, 1);">Special promotion during bazaar?</a>
+                    </div> -->
+                    
+                </div>
+
             </div>
         </div>
     </section>
